6ee361ea631c9277cf3757da6bbe49723ecd04fa
Olen Batman <t1kuki00@students.oamk.fi>
2022-11-17 17:36:22 +0200

    Trigger luotu logiin account transactionista

--- database.sql
+++ database.sql
@@ -41,9 +41,32 @@ CREATE TABLE `account` (
 
 LOCK TABLES `account` WRITE;
 /*!40000 ALTER TABLE `account` DISABLE KEYS */;
-INSERT INTO `account` VALUES (15212,1111111,'27858',1543,0),(15213,1111111,'63007',1659,0),(15214,1111111,'72996',25,0),(15215,1111111,'93993',15256,0),(15216,1111111,'61773',96244,0),(15217,1111111,'97529',16093,0),(15218,1111111,'43637',112,0),(15219,1111111,'53299',99,0),(15220,1111111,'39904',0,8400),(15221,1111111,'88537',0,756),(15222,1111111,'96590',0,2000),(15223,1111111,'10926',0,5000),(15224,1111111,'88954',0,1000),(15225,1111111,'97414',0,546),(15226,1111111,'14390',0,12),(15227,1111111,'65276',0,10000),(15228,1111111,'31640',163,0),(15229,1111111,'46258',16474,0),(15230,1111111,'79953',7845,0),(15231,1111111,'96984',256,0);
+INSERT INTO `account` VALUES (15212,1111111,'27858',1243,0),(15213,1111111,'63007',1659,0),(15214,1111111,'72996',25,0),(15215,1111111,'93993',15256,0),(15216,1111111,'61773',96244,0),(15217,1111111,'97529',16093,0),(15218,1111111,'43637',112,0),(15219,1111111,'53299',99,0),(15220,1111111,'39904',0,8300),(15221,1111111,'88537',0,756),(15222,1111111,'96590',0,1000),(15223,1111111,'10926',0,5000),(15224,1111111,'88954',0,1000),(15225,1111111,'97414',0,546),(15226,1111111,'14390',0,12),(15227,1111111,'65276',0,10000),(15228,1111111,'31640',163,0),(15229,1111111,'46258',16474,0),(15230,1111111,'79953',7845,0),(15231,1111111,'96984',256,0);
 /*!40000 ALTER TABLE `account` ENABLE KEYS */;
 UNLOCK TABLES;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_0900_ai_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+/*!50003 CREATE*/ /*!50017 DEFINER=`root`@`localhost`*/ /*!50003 TRIGGER `banklog` AFTER UPDATE ON `account` FOR EACH ROW BEGIN
+IF NEW.balance < OLD.balance THEN
+INSERT INTO log
+VALUES(NULL,NEW.idaccount,old.balance-new.balance,now());
+ELSE
+INSERT INTO log
+VALUES(NULL,NEW.idaccount,old.credit-new.credit,now());
+END IF;
+END */;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
 
 --
 -- Table structure for table `card`
@@ -91,7 +114,7 @@ CREATE TABLE `log` (
   PRIMARY KEY (`idlog`),
   KEY `idaccount_idx` (`idaccount`),
   CONSTRAINT `idaccount` FOREIGN KEY (`idaccount`) REFERENCES `account` (`idaccount`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb3;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -100,6 +123,7 @@ CREATE TABLE `log` (
 
 LOCK TABLES `log` WRITE;
 /*!40000 ALTER TABLE `log` DISABLE KEYS */;
+INSERT INTO `log` VALUES (1,15212,100,'2022-11-17 16:32:14'),(2,15212,100,'2022-11-17 16:32:49'),(3,15212,100,'2022-11-17 17:22:13'),(4,15220,100,'2022-11-17 17:22:35'),(5,15222,1000,'2022-11-17 17:22:48');
 /*!40000 ALTER TABLE `log` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -127,6 +151,10 @@ LOCK TABLES `user` WRITE;
 INSERT INTO `user` VALUES (1,'Matti','Näsä'),(2,'Juha','Koistinen'),(3,'Mauno','Ahonen'),(4,'Pertti','Keinonen'),(5,'Sakari','Östermalm'),(6,'Esko','Mörkö'),(7,'Gerhard','Rihmakallo'),(8,'Kalervo','Jankko'),(9,'Maxwell','Gothenburg'),(10,'Elmeri','Hautamäki'),(11,'Dean','Kagelberg'),(12,'James','Kagelberg'),(13,'Raili','Rasinkangas'),(14,'Eugen','von Lahtinen');
 /*!40000 ALTER TABLE `user` ENABLE KEYS */;
 UNLOCK TABLES;
+
+--
+-- Dumping routines for database 'bankdb'
+--
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -137,4 +165,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-11-15 23:38:18
+-- Dump completed on 2022-11-17 17:24:51
