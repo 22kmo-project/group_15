c25587dbb9e8445bb4f9dc110a8cfe605fa027cf
jormaliisa <vikmanhenri95@gmail.com>
2022-11-19 14:10:50 +0200

    user,card,account ja log endpointit tehty

---
+++ backend/routes/log.js
@@ -0,0 +1,53 @@
+const express = require("express");
+const router = express.Router();
+const log = require("../models/log_model");
+
+router.get("/:id?", function (request, response) {
+  if (request.params.id) {
+    log.getById(request.params.id, function (err, dbResult) {
+      if (err) {
+        response.json(err);
+      } else {
+        response.json(dbResult[0]);
+      }
+    });
+  } else {
+    log.getAll(function (err, dbResult) {
+      if (err) {
+        response.json(err);
+      } else {
+        response.json(dbResult);
+      }
+    });
+  }
+});
+
+router.post("/", function (request, response) {
+  log.add(request.body, function (err, dbResult) {
+    if (err) {
+      response.json(err);
+    } else {
+      response.json(request.body);
+    }
+  });
+});
+
+router.delete("/:id", function (request, response) {
+  log.delete(request.params.id, function (err, dbResult) {
+    if (err) {
+      response.json(err);
+    } else {
+      response.json(dbResult);
+    }
+  });
+});
+
+router.put("/:id", function (request, response) {
+  log.update(request.params.id, request.body, function (err, dbResult) {
+    if (err) {
+      response.json(err);
+    } else {
+      response.json(dbResult);
+    }
+  });
+});
