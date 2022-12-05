1ada8f021edc23b2dae342d5dcfb0e90bbf253bd
Cerveku <102629344+Cerveku@users.noreply.github.com>
2022-12-05 19:38:47 +0200

    Status päivitetty

--- frontend/status.h
+++ frontend/status.h
@@ -2,6 +2,10 @@
 #define STATUS_H
 
 #include <QDialog>
+#include <QtNetwork>
+#include <QNetworkAccessManager>
+#include <QJsonDocument>
+#include "url.h"
 
 namespace Ui {
 class status;
@@ -12,11 +16,28 @@ class status : public QDialog
     Q_OBJECT
 
 public:
-    explicit status(QWidget *parent = nullptr);
+    explicit status(QString idaccount, QByteArray webToken, QWidget *parent = nullptr);
     ~status();
+
+private slots:
+    void logSlot (QNetworkReply *reply);
+
+    void dataSlot (QNetworkReply *reply);
+
+    void on_pushButton_clicked();
 
 private:
     Ui::status *ui;
+    void getLog(QString idaccount, QByteArray webToken);
+    void getBalance(QString idaccount, QByteArray webToken);
+    QByteArray webToken;
+    QNetworkAccessManager *dataManager;
+    QNetworkReply *reply;
+    QByteArray response_data;
+    QNetworkAccessManager *logManager;
+    QStringList logitems;
+
+
 };
 
 #endif // STATUS_H
